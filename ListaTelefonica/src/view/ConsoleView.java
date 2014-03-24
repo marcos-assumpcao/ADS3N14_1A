78157fa114fa17bc94367c7b70e4a454eeea11c8
marcos-assumpcao <52.marcos.vinicius@gmail.com>
2014-03-24 17:50:16 -0300

    Class ConsoleView
    Criação da classe ConsoleView

---
+++ ListaTelefonica/src/view/ConsoleView.java
@@ -0,0 +1,12 @@
+package view;
+/**
+ * @author Marcos Assumpcao
+ *
+ */
+public class ConsoleView {
+	public void imprimePessoa(String letra, String nome, String telefone){
+		System.out.println("Letra: " + letra);
+		System.out.println("Nome: " + nome);
+		System.out.println("Telefone: " + telefone);
+	}
+}
