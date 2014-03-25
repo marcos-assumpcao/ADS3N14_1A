3337f6245b0246f70b3589dcc314b24178c6b2c7
marcos-assumpcao <52.marcos.vinicius@gmail.com>
2014-03-25 14:46:58 -0300

    Create ListaEncadeada, Nodo, Controller
    Classe Controller, Nodo, ListaEncadeada

---
+++ ListaTelefonica/src/controller/Controller.java
@@ -0,0 +1,13 @@
+package controller;
+
+import java.util.Scanner;
+
+import model.ListaEncadeada;
+import model.Nodo;
+import model.Pessoa;
+
+public class Controller{
+	Scanner leitor = new Scanner(System.in);
+	ListaEncadeada<String> lista= new ListaEncadeada<String>();
+	Nodo<String> nodo = new Nodo<String>();
+}
