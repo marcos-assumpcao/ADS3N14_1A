b8532a3cd0b9b99aed94c0d80949ceeeb941d8b0
marcos-assumpcao <52.marcos.vinicius@gmail.com>
2014-04-21 13:01:41 -0300

    Trabalho 4 - Selection Sort
    Criação da classe controller SelectionSort

---
+++ Ordenacao/src/controller/SelectionSort.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.Matriz;
+
+public class SelectionSort {
+	Matriz matriz = new Matriz();
+	int menor;
+	int indiceMenor;
+	int comparacao = 0;
+	
+	public void selectionSort(int tabela[]){
+		for (int i = 0; i < tabela.length - 1; i++) {
+            menor = tabela[i];
+            indiceMenor = i;
+
+            for (int j = i + 1; j < tabela.length; j++){
+				comparacao++;
+				matriz.setComparacao(comparacao);
+                if (tabela[j] < menor){
+                    menor = tabela[j];
+                    indiceMenor = j;
+                }
+            }
+            tabela[indiceMenor] = tabela[i];
+            tabela[i] = menor;
+        }
+	}
+}
