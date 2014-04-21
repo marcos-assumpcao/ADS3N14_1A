69f3621d303576b28bc2b49fa72062fc26d86943
marcos-assumpcao <52.marcos.vinicius@gmail.com>
2014-04-21 14:26:17 -0300

    Trabalho 4 - Alterações Main
    Alterações na classe Main.

--- Ordenacao/src/Controller/Main.java
+++ Ordenacao/src/Controller/Main.java
@@ -1,30 +1,35 @@
 package controller;
 
 import view.ViewHeap;
+import view.ViewSelection;
 
 public class Main {
 	public static void main(String[] args){
-		ViewHeap imprime = new ViewHeap();
+		ViewHeap imprimeHeap = new ViewHeap();
+		ViewSelection imprimeSelection = new ViewSelection();
 		HeapSort heapSort = new HeapSort();
 		SelectionSort selectionSort = new SelectionSort();
-		int [] numeros = new int[]{1, 7, 5, 12, 4, 2, 9, 11, 3, 8, 6, 0, 10}; 
+		
+		int [] numerosHeap = new int[]{1, 7, 5, 12, 4, 2, 9, 11, 3, 8, 6, 0, 10};
+		int [] numerosSelection = new int[]{1, 7, 5, 12, 4, 2, 9, 11, 3, 8, 6, 0, 10};
 		//HeapSort
 		System.out.println("Heap Sort");
 		System.out.println("Antes da ordenacao:");
-		imprime.ImprimeInfo(numeros);
+		imprimeHeap.ImprimeInfo(numerosHeap);
 
 		System.out.println("\n\nDepois da ordenacao:");
-		heapSort.heapSort(numeros);
-		imprime.ImprimeInfo(numeros);
-		imprime.ImprimeComp(HeapSort.matriz.getComparacao());
+		heapSort.heapSort(numerosHeap);
+		imprimeHeap.ImprimeInfo(numerosHeap);
+		imprimeHeap.ImprimeComp(HeapSort.matriz.getComparacao());
+		
 		//SelectionSort
-		System.out.println("Selection Sort");
+		System.out.println("\nSelection Sort");
 		System.out.println("Antes da ordenacao:");
-		imprime.ImprimeInfo(numeros);
+		imprimeSelection.ImprimeInfo(numerosSelection);
 
 		System.out.println("\n\nDepois da ordenacao:");
-		selectionSort.selectionSort(numeros);
-		imprime.ImprimeInfo(numeros);
-		imprime.ImprimeComp(SelectionSort.matriz.getComparacao());
+		selectionSort.selectionSort(numerosSelection);
+		imprimeSelection.ImprimeInfo(numerosSelection);
+		imprimeSelection.ImprimeComp(SelectionSort.matriz.getComparacao());
 	}
 }
