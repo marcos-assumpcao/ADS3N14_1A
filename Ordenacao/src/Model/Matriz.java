226545a6ebf644e0f4a934c801e646d7a5d51dcd
marcos-assumpcao <52.marcos.vinicius@gmail.com>
2014-04-21 02:08:16 -0300

    Trabalho 4
    Criação da classe Model Matriz

---
+++ Ordenacao/src/Model/Matriz.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class Matriz {
+	private int[] matriz;
+	private int comparacao;
+	
+	public int[] getMatriz() {
+		return matriz;
+	}
+	
+	public void setMatriz(int[] matriz) {
+		this.matriz = matriz;
+	}
+	
+	public int getComparacao() {
+		return comparacao;
+	}
+	
+	public void setComparacao(int comparacao) {
+		this.comparacao = comparacao;
+	}
+}
